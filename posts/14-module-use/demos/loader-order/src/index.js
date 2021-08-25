321a862eb54bdc894a7445d00f6525cb8d43fbf2
陈云峰 <yunfeng.chen@luckincoffee.com>
2021-08-25 18:07:49 +0800

    feat: loader执行顺序注释

--- posts/14-module-use/demos/loader-order/src/index.js
+++ posts/14-module-use/demos/loader-order/src/index.js
@@ -2,3 +2,10 @@ import "../loader/index.js?name=c2!../loader/index.js?name=c1!./hello.js"; // a1
 // import '!../loader/index.js?name=c2!../loader/index.js?name=c1!./hello.js' // a1 a2 c1 c2 d1 d2
 // import '-!../loader/index.js?name=c2!../loader/index.js?name=c1!./hello.js' // c1 c2 d1 d2
 // import '!!../loader/index.js?name=c2!../loader/index.js?name=c1!./hello.js' // c1 c2
+
+// Loaders 的執行可以被 inline 的特定的前置符所取消：
+
+// !: 取消 normal Loaders 的執行
+// -!: 取消 pre, normal Loaders 的執行
+// !!: 取消 pre, normal, post Loaders 的執行
+
