1d690a32772ebb80e653ec375f18249d911f2826
VictorSan <3292495046@qq.com>
2021-09-26 22:18:48 +0800

    feat: 在09 entry-array demo中验证导出模块的引用一致性

--- posts/09-entry/demos/entry-array/app/index2.js
+++ posts/09-entry/demos/entry-array/app/index2.js
@@ -1,3 +1,11 @@
 import msg2 from "./msg2.js";
 
+import msg from './msg.js';
+import {obj} from "./msg.js";
+
+
 console.log(msg2);
+
+msg.oo += 'uuuuu';
+console.log('index2 msg', msg);
+console.log('index2 obj.a++', obj.a++);
--- posts/09-entry/demos/entry-array/app/msg.js
+++ posts/09-entry/demos/entry-array/app/msg.js
@@ -1,1 +1,7 @@
-export default "index";
+export default {
+  oo: 'oo'
+};
+
+export const obj = {
+  a: 1,
+};
