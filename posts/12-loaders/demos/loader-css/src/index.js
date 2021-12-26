0f560427ba0d896f6c8e58e106e2f7b8035693c1
陈云峰 <yunfeng.chen@luckincoffee.com>
2021-12-26 22:23:39 +0800

    loader & 优化

--- posts/12-loaders/demos/loader-css/src/index.js
+++ posts/12-loaders/demos/loader-css/src/index.js
@@ -1,4 +1,6 @@
 import css from "./style.css";
+import {sortBy} from 'lodash';
+console.log('sortBy', sortBy);
 
 function style(cssString) {
   const element = document.createElement("style");
@@ -9,3 +11,10 @@ function style(cssString) {
 }
 
 document.head.appendChild(style(css.toString()));
+
+setTimeout(() => {
+  import('./asyncAsset').then((asyncAsset) => {
+    console.log('asyncAsset-----', asyncAsset);
+    console.log('asyncAsset.default-----', asyncAsset.default);
+  })
+}, 3000)
